18ad2367c2375718e5796478cd0db32e7afc4e8e
rabbittrix <rabbittrix@hotmail.com>
2022-06-01 21:29:38 +0100

    Testing and code cleaning

--- src/main/java/com/sysfactoring/tests/api/services/UserService.java
+++ src/main/java/com/sysfactoring/tests/api/services/UserService.java
@@ -1,10 +1,14 @@
 package com.sysfactoring.tests.api.services;
 
 import com.sysfactoring.tests.api.domain.User;
+import com.sysfactoring.tests.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
+    User update(UserDTO obj);
+    void delete(Integer id);
 }
