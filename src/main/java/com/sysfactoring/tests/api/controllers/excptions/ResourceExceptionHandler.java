5ab882b3af5d281eeeb75c9bd6160c21a3ee9ef6
rabbittrix <rabbittrix@hotmail.com>
2022-06-01 21:28:43 +0100

    Testing and code cleaning

--- src/main/java/com/sysfactoring/tests/api/controllers/excptions/ResourceExceptionHandler.java
+++ src/main/java/com/sysfactoring/tests/api/controllers/excptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sysfactoring.tests.api.controllers.excptions;
 
+import com.sysfactoring.tests.api.services.exceptions.DataIntegratyViolationException;
 import com.sysfactoring.tests.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,16 @@ import java.time.LocalDateTime;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError>ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError>objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
         StandardError error =
-                new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), e.getMessage(), request.getRequestURI());
+                new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
